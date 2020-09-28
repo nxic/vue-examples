301e6ccdce4221bfe26be40b806bbcad4dc32cd0
Yondonjamts Ganzorig <yondonjamts@outlook.com>
2020-09-28 22:32:30 +0800

    parentChild

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,12 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <ci @modalCalled="parentOpenModal($event)"/>
+    <hr>
+    <button @click.prevent="irsenDataXaray">irsenDataXaray</button>
+    <div v-for="(color, i) in colors" :key="i">
+      <div :style="`background-color:${color}`" style="border: 1px solid; margin: 10px; padding: 20px; height: 50px">{{ color }}</div>
+    </div>
   </div>
 </template>
 
@@ -11,7 +16,42 @@ import HelloWorld from './components/HelloWorld.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    'ci': HelloWorld
+  },
+  data() {
+    return {
+      pass: {
+        pasProp: new Date(),
+        pasProp1: 'string',
+      },
+      irsenData: {},
+      colors: [
+        "#000000",
+        "#FF5555",
+        "#50FA7B",
+        "#F1FA8C",
+        "#BD93F9",
+        "#FF79C6",
+        "#8BE9FD",
+        "#4D4D4D",
+        "#FF6E67",
+        "#5AF78E",
+        "#F4F99D",
+        "#CAA9FA",
+        "#FF92D0",
+        "#9AEDFE",
+        "#E6E6E6",
+      ]
+    }
+  },
+  methods: {
+    parentOpenModal(params) {
+      console.log(JSON.stringify(params));
+      this.irsenData = {...params}; // spread
+    },
+    irsenDataXaray() {
+      console.log(JSON.stringify(this.irsenData));
+    }
   }
 }
 </script>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
@@ -27,14 +26,28 @@
       <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
       <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
     </ul>
+    <button @click.prevent="childopenmodal()">childOpenModalButton</button>
   </div>
 </template>
 
 <script>
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
+  data() {
+    return {
+      dummy: [
+        { name: 'asdf', age: 1 },
+        { name: 'aoeu', age: 2 },
+        { name: 'йыбө', age: 3 },
+      ],
+      conferenceId: 1234567,
+    }
+  },
+  methods: {
+    childopenmodal() {
+      console.log('log from child');
+      this.$emit('modalCalled', { conferenceId: this.conferenceId, users: this.dummy });
+    },
   }
 }
 </script>
